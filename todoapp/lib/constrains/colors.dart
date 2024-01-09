634a515972d126989c337af507596586af492f41
ManojLL <manojlakshan421@gmail.com>
2024-01-09 12:42:54 +0530

    added search bar

--- todoapp/lib/constrains/colors.dart
+++ todoapp/lib/constrains/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color TBGcolor = Color.fromARGB(255, 111, 129, 231);
 const Color IconColor = Color.fromARGB(0, 80, 79, 79);
+const Color bgColor = Color.fromARGB(195, 230, 117, 220);
--- todoapp/lib/screens/Home.dart
+++ todoapp/lib/screens/Home.dart
@@ -7,27 +7,59 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: TBGcolor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-              Icons.menu,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset('assets/images/Logo.jpg')),
-            )
-          ],
+      backgroundColor: bgColor,
+      appBar: _AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            
+            children: [
+              _searchbar()
+            ],
+          ),
         ),
       ),
-      body: Center(
-        child: Text('This is the new screen!'),
+    );
+  }
+
+  Container _searchbar() {
+    return Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(Icons.search,size: 20,),
+                  prefixIconConstraints: BoxConstraints(maxHeight: 20,minWidth: 25),
+                  border:  InputBorder.none,
+                  hintText: "Search Text"
+                ),
+              ),
+            );
+  }
+
+  AppBar _AppBar() {
+    return AppBar(
+      backgroundColor: TBGcolor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/Logo.jpg')),
+          )
+        ],
       ),
     );
   }
